18a445d2e543d3462e0b17b67517e04e1dae7b2e
Ziad Akram Bathish <37141372+ziadba95@users.noreply.github.com>
2021-05-19 01:25:03 +0200

    add flying and losing scripts

---
+++ Project/TheFighterBird/Assets/Scripts/Fly.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public Vector2 FlyForce;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(FlyForce);
+         //   GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Project/TheFighterBird/Assets/Scripts/Lose.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lose : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 5.7)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (transform.position.y < -5)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
