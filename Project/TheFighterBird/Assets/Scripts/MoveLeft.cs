50a32830c2bc9da7360a71a366ca25cd3b1516a9
Ziad Akram Bathish <37141372+ziadba95@users.noreply.github.com>
2021-05-19 08:07:52 +0200

    Adding trees prefabs with left movement

---
+++ Project/TheFighterBird/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float Speed = 1;
+   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * Speed, Space.World);
+        if (transform.position.x < -15)
+        {
+            transform.position += new Vector3(30, 0, 0); //Vector3.right * 30
+            ShowRandomSprite();
+          //  enemy?.Respawn();
+        }
+    }
+    private void ShowRandomSprite()
+    {
+        int index = UnityEngine.Random.Range(0, 3);
+        int childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            bool shouldShow = index == i;
+            child.gameObject.SetActive(shouldShow);
+        }
+    }
+    private void OnEnable()
+    {
+        ShowRandomSprite();
+    }
+}
