844bb63a8fbdcfeb9b12c3fcc824b24f878b836b
Ziad Akram Bathish <37141372+ziadba95@users.noreply.github.com>
2021-05-19 09:59:22 +0200

    Optimizing Enemies movement

---
+++ Project/TheFighterBird/Assets/Scripts/MoveUpDown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpDown : MonoBehaviour
+{
+    private float HeightVariance = 1f;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * Mathf.Sin(Time.time) * Time.deltaTime * HeightVariance;
+    }
+}
